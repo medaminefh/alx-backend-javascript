e18fac5a4ea31297cb34e6e8902d6f3505731051
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-04-23 16:33:33 +0100

    100 task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let user;
+  let photo;
+
+  try {
+    user = await createUser();
+    photo = await uploadPhoto();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  return { photo, user };
+}
