d79b98e86c976c205dce569717b262da8d227f9c
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-06-19 07:35:30 +0100

    add js docs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,11 @@
+/**
+ * Logs a message to the console.
+ *
+ * @param {string} msg - The message to be logged.
+ * @return {undefined} This function does not return a value.
+ */
 function displayMessage(msg) {
-    process.stdout.write(msg);
+    console.log(msg);
 }
 
 module.exports = displayMessage;
